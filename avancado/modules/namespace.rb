af9c916304abd8474e0c609bc62c79c08d1fdbf3
Douglas Grund <douglasgrund@yahoo.com.br>
2024-03-18 15:48:09 -0300

    add class

--- avancado/modules/namespace.rb
+++ avancado/modules/namespace.rb
@@ -2,7 +2,14 @@ module ReverseWord
   def self.puts text
       print text.reverse.to_s
   end
+
+  class Imprimir
+    def call text
+      print text
+      print '---Imprimir---'
+    end
+  end
 end
 
-ReverseWord::puts 'O resultado é'
-puts 'O resultado é'
+imprimir = ReverseWord::Imprimir.new
+imprimir.call 'Oresultadp é'
