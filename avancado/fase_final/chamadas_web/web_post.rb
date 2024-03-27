bb22ef5a5ea8df5e3b62c054a10687e2c4e840f6
Douglas Grund <douglasgrund@yahoo.com.br>
2024-03-27 14:51:17 -0300

    erro de digitacao corrigido

--- avancado/fase_final/chamadas_web/web_post.rb
+++ avancado/fase_final/chamadas_web/web_post.rb
@@ -2,7 +2,7 @@ require 'net/http'
 
 req = Net::HTTP::Post.new("/api/users")
 # para fazer chamdas https
-re.set_form_data({ name:'Mario', job:'Encanador'})
+req.set_form_data({ name:'Mario', job:'Encanador'})
 
 response = Net::HTTP.start('reqres.in', use_ssl: true) do |http|
   http.request(req)
