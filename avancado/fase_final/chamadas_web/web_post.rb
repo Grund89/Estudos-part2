96aa9e4a6cdf19aadbc52b6c6bf857c81a116b18
Douglas Grund <douglasgrund@yahoo.com.br>
2024-03-27 14:44:02 -0300

    add code

--- avancado/fase_final/chamadas_web/web_post.rb
+++ avancado/fase_final/chamadas_web/web_post.rb
@@ -1,1 +1,12 @@
 require 'net/http'
+
+req = Net::HTTP::Post.new("/api/users")
+# para fazer chamdas https
+re.set_form_data({ name:'Mario', job:'Encanador'})
+
+response = Net::HTTP.start('reqres.in', use_ssl: true) do |http|
+  http.request(req)
+end
+
+puts response.code
+puts response.body
